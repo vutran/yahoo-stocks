12544caa23ca2f4f5f4c6eb4133b2b7089a7d237
Carlos Uribe <cm45t3r@gmail.com>
2017-08-24 11:44:18 -0500

    Set interval, start and end though options.

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ const getJson = (url) => new Promise((resolve, reject) => {
 
 const lookup = (symbol) => new Promise((resolve, reject) => {
     Promise.all([
-        getJson(`https://autoc.finance.yahoo.com/autoc?query=${symbol}&region=1&lang=en`),
-        getJson(`https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?&modules=summaryProfile,financialData`),
-    ])
+            getJson(`https://autoc.finance.yahoo.com/autoc?query=${symbol}&region=1&lang=en`),
+            getJson(`https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?&modules=summaryProfile,financialData`),
+        ])
         .then((responses) => {
             if (!responses[0].ResultSet.Result.length) {
                 reject(true);
@@ -52,13 +52,19 @@ const lookup = (symbol) => new Promise((resolve, reject) => {
 
 const history = (symbol, options) => new Promise((resolve, reject) => {
     let interval = '1m';
-    const start = moment().utc().hour(14).minute(30).second(0).millisecond(0).subtract(1, 'day');
-    const end = start.clone().hour(21).minute(0).second(0).millisecond(0);
+    let start = moment().utc().hour(14).minute(30).second(0).millisecond(0).subtract(1, 'day');
+    let end = start.clone().hour(21).minute(0).second(0).millisecond(0);
 
-    if (options && options.interval === '5d') {
-        interval = '5m';
-        start.subtract(6, 'day');
-        end.set(start);
+    if (options) {
+        interval = options.interval || interval;
+        start = options.start ? moment(options.start) : start;
+        end = options.end ? moment(options.end) : end;
+
+        if (options.interval === '5d') {
+            interval = '5m';
+            start.subtract(6, 'day');
+            end.set(start);
+        }
     }
 
     getJson(`https://query2.finance.yahoo.com/v7/finance/chart/${symbol}?period2=${end.unix()}&period1=${start.unix()}&interval=${interval}&indicators=quote&includeTimestamps=true&includePrePost=true&events=div%7Csplit%7Cearn`)
